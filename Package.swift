1e7307b1773df818c2389dc802e240fdc98266aa
jowong123 <76723162+jowong123@users.noreply.github.com>
2026-07-25 14:31:58 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GreetingKit",
-            url: "https://github.com/user-attachments/files/30342602/GreetingKit.xcframework.zip",
+            url: "https://github.com/jowong123/GreetingKit/releases/tag/1.0.0",
             checksum: "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
         )
     ]
